106028a614edb3c13ca817cef6f6f28681434144
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-08-21 16:36:53 +0600

    --------

--- script.js
+++ script.js
@@ -1,48 +1,48 @@
-const smallMemory = document.getElementById('small-memory')
-const bigMemory = document.getElementById('big-memory')
-const memoryPrice = document.getElementById('memory-price')
-const storage256GB = document.getElementById('storage-256gb')
-const storage512GB = document.getElementById('storage-512gb')
-const storage1TB = document.getElementById('storage-1tb')
-const storagePrice = document.getElementById('storage-price')
-const deliveryCost = document.getElementById('charge')
-const freeDelivery = document.getElementById('free-delivery')
-const paidDelivery = document.getElementById('paid-delivery')
-const total = document.getElementById('total')
-const macBookPrice = document.getElementById('best-price')
+const smallMemory = document.getElementById('small-memory');
+const bigMemory = document.getElementById('big-memory');
+const memoryPrice = document.getElementById('memory-price');
+const storage256GB = document.getElementById('storage-256gb');
+const storage512GB = document.getElementById('storage-512gb');
+const storage1TB = document.getElementById('storage-1tb');
+const storagePrice = document.getElementById('storage-price');
+const deliveryCost = document.getElementById('charge');
+const freeDelivery = document.getElementById('free-delivery');
+const deliveryCharge = document.getElementById('delivery-charge');
+const total = document.getElementById('total-prices');
+const macBookPrice = document.getElementById('best-prices');
 function all(type) {
     type(Cost).innerText =
         updateTotal()
 }
 //   memory cost
-memory8GB.addEventListener('click', function () {
-    memoryCost.innerText = '0'
+smallMemory.addEventListener('click', function () {
+    smallMemory.innerText = '0';
     updateTotal()
 })
-memory16GB.addEventListener('click', function () {
-    memoryCost.innerText = '180'
+bigMemory.addEventListener('click', function () {
+    bigMemory.innerText = '180';
     updateTotal()
 })
 // storage cost
 storage256GB.addEventListener('click', function () {
-    storageCost.innerText = '0'
+    storagePrice.innerText = '0';
     updateTotal()
 })
 storage512GB.addEventListener('click', function () {
-    storageCost.innerText = '100'
+    storagePrice.innerText = '100';
     updateTotal()
 })
 storage1TB.addEventListener('click', function () {
-    storageCost.innerText = '180'
+    storagePrice.innerText = '180';
     updateTotal()
 })
 // delivery cost
 freeDelivery.addEventListener('click', function () {
-    deliveryCost.innerText = '0'
+    deliveryCost.innerText = '0';
     updateTotal()
 })
-paidDelivery.addEventListener('click', function () {
-    deliveryCost.innerText = '20'
+deliveryCharge.addEventListener('click', function () {
+    deliveryCost.innerText = '20';
     updateTotal()
 })
 
@@ -59,10 +59,10 @@ paidDelivery.addEventListener('click', function () {
 //     promoCodeInput.value = ''
 // })
 function updateTotal() {
-    const bestPrice = Number(macBookPrice.innerText)
-    const extraMemoryCost = Number(memoryCost.innerText);
-    const extraStorageCost = Number(storageCost.innerText)
-    const extraDeliveryCost = Number(deliveryCost.innerText)
+    const bestPrice = Number(macBookPrice.innerText);
+    const extraMemoryCost = Number(memoryPrice.innerText);
+    const extraStorageCost = Number(storagePrice.innerText);
+    const extraDeliveryCost = Number(deliveryCost.innerText);
     const grandTotal = bestPrice + extraMemoryCost + extraStorageCost + extraDeliveryCost
-    total.innerText = grandTotal
+    total.innerText = grandTotal;
 }
