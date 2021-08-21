1f95492036034e02022b97afa9b62b54e7138bda
tonmoyasiquetamal <tonmoyasiquetamal.cse@gmail.com>
2021-08-21 23:38:28 +0600

    actually submit

--- js/script.js
+++ js/script.js
@@ -3,103 +3,102 @@ function common() {
     const bestPriceText = document.getElementById("best-price").innerText;
     const bestPrice = parseInt(bestPriceText);
 
-    const memoryText = document.getElementById("memory-cost").innerText;
-    const memoryPrice = parseInt(memoryText);
+    const ramText = document.getElementById("ram-cost").innerText;
+    const ramPrice = parseInt(ramText);
 
-    const storageText = document.getElementById("storage-cost").innerText;
-    const storagePrice = parseInt(storageText);
+    const romText = document.getElementById("rom-cost").innerText;
+    const romPrice = parseInt(romText);
 
     const deliveryText = document.getElementById("delivery-cost").innerText;
     const deliveryPrice = parseInt(deliveryText);
 
     let totalprice = document.getElementById("total-price");
-    totalprice.innerText = (bestPrice + memoryPrice + storagePrice + deliveryPrice);
-    document.getElementById("discount-price").innerText = totalprice.innerText;
+    totalprice.innerText = (bestPrice + ramPrice + romPrice + deliveryPrice);
+    document.getElementById("discount-offer").innerText = totalprice.innerText;
     return totalprice;
-}
-
-/* The Price Calculating */
-function calculatePrice(memory8, memory16, gb256, gb512, tb1, df, dc) {
-    if (gb256 = 0) {
-        common()
-    }
-    else if (gb512 = 100) {
-        common()
-    }
-    else if (tb1 = 180) {
-        common()
-    }
-
-    else if (dc = 20) {
-        common()
-    }
-    else if (df = 1) {
-        common()
-    }
-    else if (memory16 = 180) {
-        common()
-    }
-    else if (memory8 = 0) {
-        common()
-    }
 }
 
 
 /* Memory Cost Link Up */
 
 document.getElementById("ram-8gb").addEventListener("click", function () {
-    document.getElementById("memory-cost").innerText = 0;
-    calculatePrice(0, 180, 0, 0, 0, 0, 0)
+    document.getElementById("ram-cost").innerText = 0;
+    priceCounting(0, 180, 0, 0, 0, 0, 0);
 
 });
 document.getElementById("ram-16gb").addEventListener("click", function () {
-    document.getElementById("memory-cost").innerText = 180;
-    calculatePrice(0, 180, 0, 0, 0, 0, 0)
+    document.getElementById("ram-cost").innerText = 180;
+    priceCounting(0, 180, 0, 0, 0, 0, 0);
 
 });
-
 
 /* Storage Cost Link Up */
-document.getElementById("ssd-256gb").addEventListener("click", function () {
-    document.getElementById("storage-cost").innerText = 0;
-    calculatePrice(0, 0, 0, 0, 0, 0, 0)
+document.getElementById("storage-256gb").addEventListener("click", function () {
+    document.getElementById("rom-cost").innerText = 0;
+    priceCounting(0, 0, 0, 0, 0, 100);
 
 });
-document.getElementById("ssd-512gb").addEventListener("click", function () {
-    document.getElementById("storage-cost").innerText = 100;
-    calculatePrice(0, 0, 0, 100, 0, 0, 0)
+document.getElementById("storage-512gb").addEventListener("click", function () {
+    document.getElementById("rom-cost").innerText = 100;
+    priceCounting(0, 0, 0, 100, 0, 0, 0);
 
 });
 
-document.getElementById("ssd-1tb").addEventListener("click", function () {
-    document.getElementById("storage-cost").innerText = 180;
-    calculatePrice(0, 0, 0, 0, 180, 0, 0)
+document.getElementById("storage-1tb").addEventListener("click", function () {
+    document.getElementById("rom-cost").innerText = 180;
+    priceCounting(0, 0, 0, 0, 180, 0, 0);
 
 });
 
 /* Delivery Cost Link Up */
-document.getElementById("prime-delivery").addEventListener("click", function () {
+document.getElementById("free-delivery").addEventListener("click", function () {
     document.getElementById("delivery-cost").innerText = 0;
-    calculatePrice(0, 0, 0, 0, 0, 0, 20)
+    priceCounting(0, 0, 0, 0, 0, 0, 20);
 });
 document.getElementById("charge-delivery").addEventListener("click", function () {
     document.getElementById("delivery-cost").innerText = 20;
-    calculatePrice(0, 0, 0, 0, 0, 0, 20)
+    priceCounting(0, 0, 0, 0, 0, 0, 20);
 });
 
+/* The Price Calculating with Conditional Applying */
+function priceCounting(ram8gb, ram16gb, rom256gb, rom512gb, tp, db, dp) {
+    if (rom256gb = 0) {
+        common()
+    }
+    else if (rom512gb = 100) {
+        common()
+    }
+    else if (tp = 180) {
+        common()
+    }
+
+    else if (dp = 20) {
+        common()
+    }
+    else if (db = 1) {
+        common()
+    }
+    else if (ram16gb = 180) {
+        common()
+    }
+    else if (ram8gb = 0) {
+        common()
+    }
+}
+
 /* Promo-Code Link Up */
-document.getElementById("promo-code-btn").addEventListener("click", function () {
-    const promoInput = document.getElementById("promo-code-input");
+document.getElementById("promo-code").addEventListener("click", function () {
+    const promoInput = document.getElementById("promo-input");
     if (promoInput.value == "stevekaku") {
-        const totalBotomText = document.getElementById("discount-price");
-        const totalBotomPrice = parseInt(totalBotomText.innerText);
-        const newupdate = totalBotomPrice * 20 / 100;
-        const updatePrice = totalBotomPrice - newupdate
-        totalBotomText.innerText = updatePrice;
+        const totalDownText = document.getElementById("discount-offer");
+        const totalDownPrice = parseInt(totalDownText.innerText);
+        const newUpgrade = totalDownPrice * 20 / 100;
+        const upgradePrice = totalDownPrice - newUpgrade
+        totalDownText.innerText = upgradePrice;
 
     }
     /* Storage Cost Link Up */
     else {
-        alert("Shit!! What Have You Done With Promo Code!!Enter Your Correct Promo-Code") 
+        alert("Shit!! What Have You Done With Your Original Promo Code!!Enter Your Correct Promo-Code For 20% Discount"); 
     }
 });
